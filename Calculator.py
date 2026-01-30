8a98ee99a7a0bdd14eca54013fa2f05e61ed40da
hsvittali1005-sketch <hsvittali1005@gmail.com>
2026-01-30 11:13:34 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Choose an operation:")
+print("+  Addition")
+print("-  Subtraction")
+print("*  Multiplication")
+print("/  Division")
+
+operator = input("Enter operator: ")
+
+if operator == "+":
+    print("Result =", num1 + num2)
+
+elif operator == "-":
+    print("Result =", num1 - num2)
+
+elif operator == "*":
+    print("Result =", num1 * num2)
+
+elif operator == "/":
+    if num2 != 0:
+        print("Result =", num1 / num2)
+    else:
+        print("Error: Division by zero is not allowed")
+
+else:
+    print("Invalid operator")
