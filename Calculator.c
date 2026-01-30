0a7e492218cc6b3a496b7fe7be6d2bba301bb96f
hsvittali1005-sketch <hsvittali1005@gmail.com>
2026-01-30 11:11:47 +0530

    Create Calculator.c

---
+++ Calculator.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2;
+    char operator;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    printf("Choose an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    switch (operator) {
+        case '+':
+            printf("Result = %.2lf\n", num1 + num2);
+            break;
+
+        case '-':
+            printf("Result = %.2lf\n", num1 - num2);
+            break;
+
+        case '*':
+            printf("Result = %.2lf\n", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                printf("Result = %.2lf\n", num1 / num2);
+            else
+                printf("Error: Division by zero is not allowed.\n");
+            break;
+
+        default:
+            printf("Invalid operator!\n");
+    }
+
+    return 0;
+}
